38f68dcff096953e81de2fa0df0a7ec66fbf6708
Eleanor.Davidson <michaelbdavidson7@gmail.com>
2018-04-30 01:11:44 -0400

    style

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
                 </div>
               </nav>
             </div>
-    <div class="container">
+    <div class="container-fluid">
       <router-view class="view"></router-view>
     </div>
   </div>
